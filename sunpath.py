6c28e9022e4e46518bca70360e910277800bab95
Kevin Chiu <kevin@kevinchiu.org>
2009-04-18 19:57:26 -0400

    prints out altitudes and azimuths

--- sunpath.py
+++ sunpath.py
@@ -9,12 +9,17 @@ Created by kevin on 2009-04-18.
 import sys
 import getopt
 import os
+import re
 
 help_message = '''
-Month Day Time Timezone Lat Lon
+Month Day Timezone Lat Lon
 example:
-7 4 14:30 EDT 42 89
+7 4 EDT 42 89
 '''
+
+altitudes = []
+azimuths = []
+p = re.compile('# Solar altitude and azimuth: (\-?\d*?\.\d*?)\s*?(\-?\d*?\.\d*?)')
 
 class Usage(Exception):
 	def __init__(self, msg):
@@ -23,20 +28,33 @@ class Usage(Exception):
 def main(argv=None):
 	argv = sys.argv
 
-	if len(argv) != 7:
+	if len(argv) != 6:
 		print help_message
 		exit(1)
 
 	month 	= argv[1]
 	day 		= argv[2]
-	time 		= argv[3]
-	zone		= argv[4]
-	lat			= argv[5]
-	lon			= argv[6]
+	zone		= argv[3]
+	lat			= argv[4]
+	lon			= argv[5]
 	
-	command = './gensky ' + month + ' ' + day + ' ' + time + zone + ' +s -a' + lat + ' -o' + lon 
-	result = os.popen(command).read()
-	return result
+	for hour in range(24):
+		command = './gensky ' + month + ' ' + day + ' ' + str(hour) + ':00' +  zone + ' +s -a' + lat + ' -o' + lon 
+		result = os.popen(command).read()
+		print result
+		m = p.search(result)
+		if m != None:
+			alt = m.group(1)
+			az = m.group(2)
+			altitudes.append(alt)
+			azimuths.append(az)
+			print m.group(0)
+			print alt
+			print az
+	print altitudes
+	print azimuths
+	exit(0)
+		
 
 if __name__ == "__main__":
 	sys.exit(main())
